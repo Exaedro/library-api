b529a377fa45b811007f675fe2bf68619953a887
exaedro <77593869+Exaedro@users.noreply.github.com>
2023-10-17 18:45:50 +0000

    Update 3 files

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,19 +1,27 @@
 import BooksModel from "../models/local-system/booksModel.js";
 
 export default class BooksController {
-    static async getAll(req, res) {
-        const { title, genre } = req.query;
-        const books = await BooksModel.getAll(title, genre);
+  static async getAll(req, res) {
+    const { title, genre, year } = req.query;
+    try {
+      const books = await BooksModel.getAll(title, genre, year);
 
-        if(books) return res.json(books);
-        else res.status(404).json({ message: 'Not found' });
+      if (books) return res.json(books);
+      else res.status(404).json({ message: "Not found" });
+    } catch (e) {
+      res.status(505).json({ message: "Error on server", error: e });
     }
+  }
 
-    static async getByIsbn(req, res) {
-        const { isbn } = req.params;
-        const book = await BooksModel.getByIsbn(isbn);
-        
-        if(book) return res.json(book);
-        else res.status(404).json({ message: 'Not found' });
+  static async getByIsbn(req, res) {
+    const { isbn } = req.params;
+    try {
+      const book = await BooksModel.getByIsbn(isbn);
+
+      if (book) return res.json(book);
+      else res.status(404).json({ message: "Not found" });
+    } catch (e) {
+      res.status(500).json({ message: "Error on server", error: e });
     }
+  }
 }
--- src/models/local-system/booksModel.js
+++ src/models/local-system/booksModel.js
@@ -1,21 +1,31 @@
-import data from '../../db/library.json' assert { type: 'json' };
+import data from "../../db/library.json" assert { type: "json" };
 
 export default class BooksModel {
-    static async getAll(title, genre) {
-        if(title) {
-            const bookFound = data.library.find(elem => elem.book.title.toLowerCase() == title.toLowerCase());
-            return bookFound;
-        }
-        if(genre) {
-            const booksFiltered = data.library.filter(elem => elem.book.genre.toLowerCase() == genre.toLowerCase());
-            return booksFiltered;
-        }
+  static async getAll(title, genre, year) {
+    if (title) {
+      const bookFound = data.library.find(
+        (elem) => elem.book.title.toLowerCase() == title.toLowerCase(),
+      );
+      return bookFound;
+    }
+    if (genre) {
+      const booksFiltered = data.library.filter(
+        (elem) => elem.book.genre.toLowerCase() == genre.toLowerCase(),
+      );
+      return booksFiltered;
+    }
+    if (year) {
+      const booksFiltered = data.library.filter(
+        (elem) => elem.book.year == year,
+      );
+      return booksFiltered;
+    }
 
-        return data.library;
-    }
-    
-    static async getByIsbn(isbn) {
-        const bookFound = data.library.find(elem => elem.book.ISBN == isbn);
-        return bookFound;
-    }
+    return data.library;
+  }
+
+  static async getByIsbn(isbn) {
+    const bookFound = data.library.find((elem) => elem.book.ISBN == isbn);
+    return bookFound;
+  }
 }
